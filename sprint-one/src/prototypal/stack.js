43fffb3d3d68320f5aca35cd027a7792d5495af8
baoducng <baoducng@gmail.com>
2014-06-05 10:32:18 -0700

    Completed pseudoclassical and re-wrote prototypal stack and queue

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,31 +1,34 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-var someInstance = Object.create(stackMethods);
-someInstance._storage = {};
-someInstance._size = 0;
-return someInstance;
+  var someInstances = Object.create(stackMethods);
+  someInstances._storage = {};
+  someInstances._size = 0;
+  return someInstances;
+
 };
 
 var stackMethods = {};
 
-stackMethods.push = function(value) {
+stackMethods.push = function(value){
   this._storage[this._size] = value;
   this._size++;
+  //this._storage[]
 };
 
-stackMethods.pop = function() {
-  if (this._size > 0) {
+stackMethods.pop = function(){
+  if (this._size > 0){
     this._size--;
-    var result = this._storage[this._size];
-    delete result;
+    var temp;
+    temp = this._storage[this._size];
+    delete this._storage[this._size];
   }
-  return result;
-};
+  return temp;
+}
 
-stackMethods.size = function() {
+stackMethods.size = function(){
   return this._size;
-};
+}
 
 
 
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,7 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this._storage = {};
+  this._size = 0;
+};
 
+Queue.prototype.enqueue = function(value) {
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+Queue.prototype.dequeue = function() {
+  if(this._size > 0) {
+    this._size--;
+    var temp = this._storage[0];
+    delete this._storage[0];
+    for(var index in this._storage) {
+      this._storage[index - 1] = this._storage[index];
+    }
+    return temp;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this._size;
 };
 
 
