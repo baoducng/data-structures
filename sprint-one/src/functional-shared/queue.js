260e071da2981d9ed9bde439b892771daea8b220
baoducng <baoducng@gmail.com>
2014-06-04 20:34:21 -0700

    Completed functional-shared stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,35 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance._storage = {};
+  someInstance._size = 0;
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
+
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+queueMethods.dequeue = function() {
+  if(this._size > 0) {
+    this._size--;
+    var result = this._storage[0];
+    delete result;
+    for (var index in this._storage) {
+      this._storage[index - 1] = this._storage[index];
+    }
+    return result;
+  }
+};
+
+queueMethods.size = function() {
+  return this._size;
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,43 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.storage={};
+  stack.length= 0;
+
+  _.extend(stack, stackMethods);
+  return stack;
 };
 
 var stackMethods = {};
 
+
+stackMethods.push = function(value) {
+  this.storage[this.length] = value;
+  this.length++;
+
+}
+stackMethods.pop = function() {
+  if (this.length > 0){
+    this.length--;
+    var temp;
+    temp = this.storage[this.length];
+    delete this.storage[this.length];
+    return temp;
+  }
+
+
+}
+stackMethods.size = function() {
+  return this.length;
+}
+
+
+
+//create'. objects that holds the methods for all instances
+
+//use this
+
+//extend functinon.
+//
 
