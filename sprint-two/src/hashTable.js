97f30e8be5037741911893300ff77bcbd3907dd3
baoducng <baoducng@gmail.com>
2014-06-06 19:28:28 -0700

    Completed hashtable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,19 +5,47 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if(!this._storage[i]) {
+    this._storage[i] = [];
+    this._storage[i].push(tupleMaker(k, v));
+  }else{
+    if (this._storage[i][0] === k){
+      this._storage[i][1] = v;
+    }else{
+      this._storage[i].push(tupleMaker(k, v));
+    }
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  //if its empty
+  if (!this._storage[i]){
+    return this._storage[i];
+  }else{
+    for (var index =0; index < this._storage[i].length; index++){
+      if (this._storage[i][index][0] === k){
+        return this._storage[i][index][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+ var i = getIndexBelowMaxForKey(k, this._limit);
+ this._storage[i] = null;
 };
 
+var tupleMaker = function(k, v) {
+  var tuple = [];
+  tuple.push(k);
+  tuple.push(v);
+  return tuple;
+}
 
-
+//tuple is an array with two elements where the first
+//[cat, happy] [cat, unhappy]
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,35 +1,25 @@
 var makeTree = function(value){
   var newTree = Object.create(treeMethods);
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-  if (this.children === undefined){
-    this.children = [];
-  }
   this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
-  var found = false;
-
   if(this.value === target){
-    found = true;
+    return true;
   }
-  if(this.children !== undefined){
-    for (var i = 0; i < this.children.length; i++){
-      found = this.children[i].contains(target);
-      if (found) {
-        return found;
-      }
+  for (var i = 0; i < this.children.length; i++){
+    if( this.children[i].contains(target))
+      return true;
     }
-  }
-  return found;
-
+  return false;
 };
 
 
